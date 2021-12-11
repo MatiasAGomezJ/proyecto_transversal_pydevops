1072997dcc3ef682287f40f8c9bedf8a6e020f26
Juan Carlos Mendoza Bula <jmendoza@cifpfbmoll.eu>
2021-12-11 06:04:10 +0100

    refactor: Añadir codigo para crear la carpeta en la cual se almacenaran los archivos

--- logica/src/crear_archivos_markdown.py
+++ logica/src/crear_archivos_markdown.py
@@ -1,4 +1,12 @@
+import os
+
+
 def crear_archivos_markdown(texto_para_mardown, nombre_archivo):
-    archivo = open("./markdowns/" + nombre_archivo + ".md", 'w', encoding="utf-8")
+    ruta = "./markdowns/"
+
+    if not os.path.isdir(ruta):
+        os.mkdir(ruta)
+
+    archivo = open(ruta + nombre_archivo + ".md", 'w', encoding="utf-8")
     archivo.write(texto_para_mardown)
     archivo.close()
